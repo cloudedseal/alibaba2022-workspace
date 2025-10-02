466707ff4e5eaa643d8ef7f0deb5aba870491220
cloudedseal <2652207782@qq.com>
2025-10-02 21:05:50 +0800

    spring-cloud-config via config file

--- 05-gateway-config-9000/src/main/java/com/abc/ConfigGateway9000.java
+++ 05-gateway-config-9000/src/main/java/com/abc/ConfigGateway9000.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "com.abc")
-public class ConfigGateway {
+public class ConfigGateway9000 {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfigGateway.class, args);
+        SpringApplication.run(ConfigGateway9000.class, args);
     }
 
 }
