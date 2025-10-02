caf76a4776ccccc1c689ea4be7b816fda9d4db7b
cloudedseal <2652207782@qq.com>
2025-10-02 21:06:32 +0800

    自定义路由断言工厂，解决如微服务之间需要认证的问题。

--- 05-gateway-config-9000/src/main/java/com/abc/custom/AuthRoutePredicateFactory.java
+++ 05-gateway-config-9000/src/main/java/com/abc/custom/AuthRoutePredicateFactory.java
@@ -1,4 +1,71 @@
 package com.abc.custom;
 
-public class AuthRoutePredicateFactory {
+import org.springframework.cloud.gateway.handler.predicate.AbstractRoutePredicateFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+/**
+ * 当请求头中携带有用户名与密码的 key-value 对，且其用户名与配置文件中 Auth 路由断
+ * 言工厂中指定的 username 相同，密码中包含 Auth 路由断言工厂中指定的 password 时才能
+ * 通过认证，允许访问系统。
+ * 功能前缀 Auth
+ * 固定 RoutePredicateFactory
+ */
+@Component
+public class AuthRoutePredicateFactory  extends AbstractRoutePredicateFactory<AuthRoutePredicateFactory.Config> {
+
+    public AuthRoutePredicateFactory() {
+        super(Config.class);
+    }
+
+    @Override
+    public List<String> shortcutFieldOrder() {
+        return Arrays.asList("username", "password");
+    }
+
+    @Override
+    public Predicate<ServerWebExchange> apply(Config config) {
+        return exchange -> {
+            HttpHeaders headers = exchange.getRequest().getHeaders();
+            List<String> pwds = headers.get(config.getUsername());
+            if (pwds != null && pwds.contains(config.getPassword())) {
+                return true;
+            }
+            return false;
+        };
+    }
+
+    public static class Config {
+        private String username;
+        private String password;
+
+        public Config() {
+        }
+
+        public Config(String username, String password) {
+            this.username = username;
+            this.password = password;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+    }
 }
--- 05-gateway-config-9000/src/main/java/com/abc/custom/TokenRoutePredicateFactory.java
+++ 05-gateway-config-9000/src/main/java/com/abc/custom/TokenRoutePredicateFactory.java
@@ -1,4 +1,59 @@
 package com.abc.custom;
 
-public class TokenRoutePredicateFactory {
+import org.springframework.cloud.gateway.handler.predicate.AbstractRoutePredicateFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.server.ServerWebExchange;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+/**
+ * 当请求中携带有一个 token 请求参数，且参数值包含配置文件中 Token 路由断言工厂指
+ * 定的 token 值时才能通过认证，允许访问系统。
+ */
+@Component
+public class TokenRoutePredicateFactory extends AbstractRoutePredicateFactory<TokenRoutePredicateFactory.Config> {
+
+    public TokenRoutePredicateFactory() {
+        super(Config.class);
+    }
+
+    @Override
+    public Predicate<ServerWebExchange> apply(Config config) {
+        return exchange -> {
+            MultiValueMap<String, String> queryParams = exchange.getRequest().getQueryParams();
+            List<String> values = queryParams.get("token");
+            if (values != null && values.contains(config.getToken())) {
+                return true;
+            }
+            return false;
+        };
+    }
+
+    @Override
+    public List<String> shortcutFieldOrder() {
+        return Arrays.asList("token");
+    }
+
+
+    public static class Config {
+        private String token;
+
+        public Config() {
+        }
+
+        public Config(String token) {
+            this.token = token;
+        }
+
+        public String getToken() {
+            return token;
+        }
+
+        public void setToken(String token) {
+            this.token = token;
+        }
+    }
 }
