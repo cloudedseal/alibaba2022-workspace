2edf318ab3e2719a934e56d92b5a9d37a8a92324
cloudedseal <2652207782@qq.com>
2025-10-03 16:17:58 +0800

    pre & post filter

---
+++ 05-gateway-config-9000/src/main/java/com/abc/custom/prepost/OneGatewayFilterFactory.java
@@ -0,0 +1,31 @@
+package com.abc.custom.prepost;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractNameValueGatewayFilterFactory;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+public class OneGatewayFilterFactory extends AbstractNameValueGatewayFilterFactory {
+    Logger logger = LoggerFactory.getLogger(OneGatewayFilterFactory.class);
+    @Override
+    public GatewayFilter apply(NameValueConfig config) {
+        return ((exchange, chain) -> {
+            long start = System.currentTimeMillis();
+            logger.info(config.getName() + "-" + config.getValue() + "startTime:" + start);
+            exchange.getAttributes().put("startTime", start);
+            return chain.filter(exchange).then(
+                    Mono.fromRunnable(() -> {
+                        Object startTime = exchange.getAttributes().get("startTime");
+                        long endTime = System.currentTimeMillis();
+                        exchange.getAttributes().put("endTime", endTime);
+                        long elapsedTime = endTime - start;
+                        logger.info(config.getName() + "-" + config.getValue() + "elapsedTime:" + elapsedTime);
+                        logger.info("one:" + elapsedTime);
+                    })
+            );
+        });
+    }
+}
