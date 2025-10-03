306640f91ad7be93361ca1da57423aad8aff4b93
cloudedseal <2652207782@qq.com>
2025-10-03 13:38:50 +0800

    AddRequestHeadersIfNotPresent

--- 05-show-info-8080/show-info-8080/src/main/java/com/abc/showinfo8080/SomeController.java
+++ 05-show-info-8080/show-info-8080/src/main/java/com/abc/showinfo8080/SomeController.java
@@ -4,6 +4,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Enumeration;
+
 @RestController
 @RequestMapping("/info")
 public class SomeController {
@@ -13,5 +15,23 @@ public class SomeController {
         String s = "X-Request-Color";
         String header = request.getHeader(s);
         return s + " " + header;
+    }
+
+    @RequestMapping("/allHeader")
+    public String allHeaderHandle(HttpServletRequest request) {
+        // all request headers
+        Enumeration<String> headerNames = request.getHeaderNames();
+
+        StringBuilder sb = new StringBuilder();
+        while (headerNames.hasMoreElements()) {
+            String name = headerNames.nextElement();
+            sb.append(name).append(": ");
+            Enumeration<String> headers = request.getHeaders(name);
+            while (headers.hasMoreElements()) {
+                sb.append(headers.nextElement()).append(", ");
+            }
+            sb.append("<br>");
+        }
+        return sb.toString();
     }
 }
